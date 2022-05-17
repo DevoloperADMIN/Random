eaac0f0d0611c1af393964a7a1612eb6e3c5dd86
Developer ADMIN <103903020+DevoloperADMIN@users.noreply.github.com>
2022-05-17 03:57:06 +0200

    Add files via upload

---
+++ Developer.py
@@ -0,0 +1,11 @@
+import os
+os.system ('clear')
+
+print ("\033[1;31mWalcome \033[1;30mTo \033[1;33mScript \033[1;32mADMIN")
+
+ADMIN = "\033[1;31m"
+print (ADMIN)
+Developer = 100000
+while Developer < 10000000 :
+    Developer = Developer + 1
+    print (Developer)
